4d553843e7fc2673611afa16996ca4395acdc82c
teemueer <tteerola@gmail.com>
2017-05-17 10:19:18 +0300

    Add files via upload

--- packet.py
+++ packet.py
@@ -2,50 +2,50 @@ import struct
 from StringIO import StringIO
 
 class Packet(StringIO):
-	def putByte(self, val):
-		self.write(struct.pack('<B', val))
+    def putByte(self, val):
+        self.write(struct.pack('<B', val))
 
-	def getByte(self):
-		try: return struct.unpack('<B', self.read(1))[0]
-		except: return 0
+    def getByte(self):
+        try: return struct.unpack('<B', self.read(1))[0]
+        except: return 0
 
-	def putShort(self, val):
-		self.write(struct.pack('<h', val))
+    def putShort(self, val):
+        self.write(struct.pack('<h', val))
 
-	def getShort(self, network=False):
-		if not network:
-			return struct.unpack('<h', self.read(2))[0]
-		else:
-			return struct.unpack('!H', self.read(2))[0]
+    def getShort(self, network=False):
+        if not network:
+            return struct.unpack('<h', self.read(2))[0]
+        else:
+            return struct.unpack('!H', self.read(2))[0]
 
-	def putLong(self, val):
-		self.write(struct.pack('<l', val))
+    def putLong(self, val):
+        self.write(struct.pack('<l', val))
 
-	def getLong(self):
-		try: return struct.unpack('<l', self.read(4))[0]
-		except: return 0
+    def getLong(self):
+        try: return struct.unpack('<l', self.read(4))[0]
+        except: return 0
 
-	def getLongLong(self):
-		try: return struct.unpack('<Q', self.read(8))[0]
-		except: return 0
+    def getLongLong(self):
+        try: return struct.unpack('<Q', self.read(8))[0]
+        except: return 0
 
-	def putFloat(self, val):
-		self.write(struct.pack('<f', val))
+    def putFloat(self, val):
+        self.write(struct.pack('<f', val))
 
-	def getFloat(self):
-		try: return struct.unpack('<f', self.read(4))[0]
-		except: return 0
+    def getFloat(self):
+        try: return struct.unpack('<f', self.read(4))[0]
+        except: return 0
 
-	def putString(self, val):
-		self.write(val + '\x00')
+    def putString(self, val):
+        self.write(val + '\x00')
 
-	def getString(self):
-		try:
-			val = self.getvalue()
-			start = self.tell()
-			end = val.index('\0', start)
-			val = val[start:end]
-			self.seek(end+1)
-			return val
-		except:
-			return 0
+    def getString(self):
+        try:
+            val = self.getvalue()
+            start = self.tell()
+            end = val.index('\0', start)
+            val = val[start:end]
+            self.seek(end+1)
+            return val
+        except:
+            return 0
--- server.py
+++ server.py
@@ -2,106 +2,106 @@ from query import Query
 from packet import Packet
 
 class Server(Query):
-	def get_info(self):
-		self.connect()
-		
-		packet = Packet()
-		packet.putLong(-1) # Whole
-		packet.putByte(0x54) # Header
-		packet.putString('Source Engine Query') # Payload
-		
-		self.udp.send(packet.getvalue())
-		packet = self.receive()
+    def get_info(self):
+        self.connect()
+        
+        packet = Packet()
+        packet.putLong(-1) # Whole
+        packet.putByte(0x54) # Header
+        packet.putString('Source Engine Query') # Payload
+        
+        self.udp.send(packet.getvalue())
+        packet = self.receive()
 
-		header = packet.getByte()
-		result = {}
+        header = packet.getByte()
+        result = {}
 
-		if header == 0x49: # Normal response
-			result['protocol'] = packet.getByte()
-			result['hostname'] = packet.getString()
-			result['map'] = packet.getString()
-			result['gamedir'] = packet.getString().lower()
-			result['gamedesc'] = packet.getString()
-			appid = packet.getShort()
-			result['numplayers'] = packet.getByte()
-			result['maxplayers'] = packet.getByte()
-			result['numbots'] = packet.getByte()
-			result['servertype'] = chr(packet.getByte())
-			result['os'] = chr(packet.getByte())
-			result['passworded'] = packet.getByte()
-			result['secure'] = packet.getByte()
-		
-		elif header == 0x6D: # Obsolete Goldsource response
-			result['address'] = packet.getString()
-			result['hostname'] = packet.getString()
-			result['map'] = packet.getString()
-			result['gamedir'] = packet.getString().lower()
-			result['gamedesc'] = packet.getString()
-			result['numplayers'] = packet.getByte()
-			result['maxplayers'] = packet.getByte()
-			result['protocol'] = packet.getByte()
-			result['servertype'] = chr(packet.getByte())
-			result['os'] = chr(packet.getByte())
-			result['passworded'] = packet.getByte()
-			if packet.getByte(): # Is Half-Life mod
-				packet.getString() # Link
-				packet.getString() # Download Link
-				packet.getByte() # NULL
-				packet.getLong() # Version
-				packet.getLong() # Size
-				packet.getByte() # Type
-				packet.getByte() # DLL
-			result['secure'] = packet.getByte()
-			result['numbots'] = packet.getByte()
+        if header == 0x49: # Normal response
+            result['protocol'] = packet.getByte()
+            result['hostname'] = packet.getString()
+            result['map'] = packet.getString()
+            result['gamedir'] = packet.getString().lower()
+            result['gamedesc'] = packet.getString()
+            appid = packet.getShort()
+            result['numplayers'] = packet.getByte()
+            result['maxplayers'] = packet.getByte()
+            result['numbots'] = packet.getByte()
+            result['servertype'] = chr(packet.getByte())
+            result['os'] = chr(packet.getByte())
+            result['passworded'] = packet.getByte()
+            result['secure'] = packet.getByte()
+        
+        elif header == 0x6D: # Obsolete Goldsource response
+            result['address'] = packet.getString()
+            result['hostname'] = packet.getString()
+            result['map'] = packet.getString()
+            result['gamedir'] = packet.getString().lower()
+            result['gamedesc'] = packet.getString()
+            result['numplayers'] = packet.getByte()
+            result['maxplayers'] = packet.getByte()
+            result['protocol'] = packet.getByte()
+            result['servertype'] = chr(packet.getByte())
+            result['os'] = chr(packet.getByte())
+            result['passworded'] = packet.getByte()
+            if packet.getByte(): # Is Half-Life mod
+                packet.getString() # Link
+                packet.getString() # Download Link
+                packet.getByte() # NULL
+                packet.getLong() # Version
+                packet.getLong() # Size
+                packet.getByte() # Type
+                packet.getByte() # DLL
+            result['secure'] = packet.getByte()
+            result['numbots'] = packet.getByte()
 
-		return result
+        return result
 
-	def get_challenge(self):
-		packet = Packet()
-		packet.putLong(-1) # Whole
-		packet.putByte(0x55) # Header
-		packet.putLong(-1) # Challenge
+    def get_challenge(self):
+        packet = Packet()
+        packet.putLong(-1) # Whole
+        packet.putByte(0x55) # Header
+        packet.putLong(-1) # Challenge
 
-		self.udp.send(packet.getvalue())
-		packet = self.receive()
-		
-		header = packet.getByte()
-		challenge = None
-		
-		if header == 0x41:
-			challenge = packet.getLong()
-		
-		return challenge
+        self.udp.send(packet.getvalue())
+        packet = self.receive()
+        
+        header = packet.getByte()
+        challenge = None
+        
+        if header == 0x41:
+            challenge = packet.getLong()
+        
+        return challenge
 
-	def get_players(self):
-		self.connect()
-		
-		challenge = self.get_challenge()
-		result = []
-		
-		if challenge:
-			packet = Packet()
-			packet.putLong(-1) # Whole
-			packet.putByte(0x55) # Header
-			packet.putLong(challenge)
-			
-			self.udp.send(packet.getvalue())
-			packet = self.receive()
-			
-			header = packet.getByte()
-			
-			if header == 0x44:
-				numplayers = packet.getByte()
-				try:
-					for x in xrange(numplayers):
-						player = {}
-						packet.getByte() # Index
-						player['name'] = packet.getString()
-						player['score'] = packet.getLong()
-						player['duration'] = int(packet.getFloat())
-						result.append(player)
-				except:
-					pass
+    def get_players(self):
+        self.connect()
+        
+        challenge = self.get_challenge()
+        result = []
+        
+        if challenge:
+            packet = Packet()
+            packet.putLong(-1) # Whole
+            packet.putByte(0x55) # Header
+            packet.putLong(challenge)
+            
+            self.udp.send(packet.getvalue())
+            packet = self.receive()
+            
+            header = packet.getByte()
+            
+            if header == 0x44:
+                numplayers = packet.getByte()
+                try:
+                    for x in xrange(numplayers):
+                        player = {}
+                        packet.getByte() # Index
+                        player['name'] = packet.getString()
+                        player['score'] = packet.getLong()
+                        player['duration'] = int(packet.getFloat())
+                        result.append(player)
+                except:
+                    pass
 
-		return result
-		
+        return result
+        
